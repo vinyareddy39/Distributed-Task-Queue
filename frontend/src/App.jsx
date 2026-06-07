673321cb8a043352dc5aa3ca878953c2c7bcc25f
vinyareddy39 <vinyareddy39@gmail.com>
2026-06-07 20:59:19 +0530

    Fix DashBoard import case sensitivity for Linux build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/DashBoard";
 import EditTask from "./pages/EditTask";
 import TaskDetails from "./pages/TaskDetails";
 
